03527a3eefba7ad755ae224ab3e455e8b8a0e30f
JsonSept <jasonseptember.za@gmail.com>
2023-12-04 21:46:17 +0200

    added a button

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 let add = document.querySelector('h1');
 add.innerText = 'Javascript';
+
+let btn = document.createElement('button');
+btn.innerText = "Click";
+document.body.appendChild(btn);
+
+
+
